e5c0293b81a46e63c66ce5dbd7c5c3ccca6791e3
GuerreroBryan <bryan.guerrero01@epn.edu.ec>
2019-04-20 23:10:10 -0500

    ejercicio 5

--- ejercicio5/index.php
+++ ejercicio5/index.php
@@ -1,5 +1,21 @@
 <?php
 
+$aleatorio = rand(1,100);
+$numero = 50;
+
+echo $aleatorio;	
+echo "<br>";
+
+if ($aleatorio < $numero) {
+    echo "$aleatorio es menor que $numero";
+    echo "<br>";
+		}elseif ($aleatorio == $numero){
+    	echo "$aleatorio es igual que $numero";
+    	echo "<br>";
+		}else{
+    		echo "$aleatorio es mayor que $numero";
+}
+
 /**
  Sabiendo que la función rand nos retorna un valor aleatorio entre un rango de dos enteros:
 
